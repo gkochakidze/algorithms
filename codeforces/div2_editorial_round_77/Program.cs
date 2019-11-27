7ec49080f0940b95d0aa49a6559768f97aad40b2
Giorgi Kochakidze <gikochak@microsoft.com>
2019-11-27 15:05:58 +0200

    create project div2 editorial round 77

---
+++ codeforces/div2_editorial_round_77/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace div2_editorial_round_77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
